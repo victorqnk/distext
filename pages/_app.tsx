0e6f86974c445b53aa93c145ff87625f7dc4aaa4
Victor Hugo <victor@hazmiapp.com>
2022-05-31 09:56:42 -0500

    mobile first

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,10 +6,10 @@ import Link from 'next/link'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <header className='container mx-auto h-24'>
+      <header className='md:container md:mx-auto md:px-0 px-4 h-24'>
         <div className="flex justify-between mt-2">
           <Image src='/logo-distext.jpg' width={100} height={80} />
-          <nav className='flex gap-6 my-auto'>
+          <nav className='flex gap-6 my-auto text-slate-500'>
             <Link href='/'>Inicio</Link>
             <Link href='/tienda'>Tienda</Link>
             <Link href='/contacto'>Contacto</Link>
@@ -17,7 +17,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         </div>
       </header>
       <Component {...pageProps} />
-      <footer className='bg-blue-800 w-full'>
+      <footer className='bg-blue-800 w-full px-4'>
         <div className="container mx-auto flex justify-between py-10">
           <p className='text-blue-400'>{new Date().getFullYear()} Grupo Distext</p>
           <p className='text-blue-400'>💻 Desarrollado por <span className="text-white font-bold">hazmiapp</span></p>
--- pages/index.tsx
+++ pages/index.tsx
@@ -14,11 +14,11 @@ const Home: NextPage = () => {
 
       <div className="h-screen bg-white">
         <section className="w-full bg-blue-800 bg-hero-image bg-fixed bg-cover flex justify-center h-3/4">
-          <div className="my-auto text-center">
-            <p className='text-blue-500 text-7xl font-light'>Trabajamos para</p>
-            <p className='text-white text-7xl'>Uniformar a tu empresa</p>
+          <div className="my-auto text-center font-normal">
+            <p className='text-blue-500 md:text-7xl text-5xl'>Trabajamos para</p>
+            <p className='text-white md:text-7xl text-5xl'>Uniformar a tu empresa</p>
             <div className="bg-blue-800 uppercase rounded-full w-48 my-10 block mx-auto p-3 cursor-pointer">
-              <span className="text-lg text-white font-bold">saber más</span>
+              <span className="text-lg text-white">saber más</span>
             </div>
             <span className="material-icons text-white" style={{ fontSize: '48px' }}>south</span>
           </div>
@@ -27,10 +27,10 @@ const Home: NextPage = () => {
 
       <section className="container mx-auto text-center mb-48">
         <h1 className='text-5xl text-blue-900 mb-5'>Grupo Distext</h1>
-        <p className='text-4xl w-2/3 mx-auto font-light'>Somos una empresa mexicana dedicada a la fabricación de uniformes industriales, casuales, de seguridad, médicos y personalizados.</p>
+        <p className='md:text-4xl text-3xl w-2/3 mx-auto font-light'>Somos una empresa mexicana dedicada a la fabricación de uniformes industriales, casuales, de seguridad, médicos y personalizados.</p>
       </section>
 
-      <section className="container mx-auto flex gap-6 text-center mb-12">
+      <section className="container mx-auto grid grid-cols-2 md:grid-cols-4 gap-6 text-center mb-12">
         <div>
           <span className="material-icons text-blue-600" style={{ fontSize: '72px' }}>published_with_changes</span>
           <h3 className='text-blue-900 font-bold text-xl my-2'>Personalización</h3>
